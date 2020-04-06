29c2f5b052d6715c3a0f7bef27cf5c7f9daa21ea
chey-to-mozg <denis38425@mail.ru>
2020-04-06 16:01:39 +0300

    Update main.py

--- pipline/main.py
+++ pipline/main.py
@@ -1,2 +1,18 @@
+import sys
+
+
+def compare(num):
+	num = int(num)
+	if num > 0:
+		print("GREATER")
+	elif num < 0:
+		print("LOWER")
+	else:
+		print("ZERO")
+
+
 if __name__ == "__main__":
-    print ("Hello world")
+	actions = {'compare': compare}
+	action = sys.argv[1]
+	args = sys.argv[2:]
+	actions[action](*args)
